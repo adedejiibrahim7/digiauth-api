9c7b9cd33dcbfe8ce6a2203d050420f4b98f7903
Ibrahim Adedeji <adedejiibrahim7@gmail.com>
2021-05-06 20:14:15 +0100

    index update 2

--- models/index.js
+++ models/index.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'production' ;
+const env = 'production';
+// const env = process.env.NODE_ENV || 'production' ;
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
